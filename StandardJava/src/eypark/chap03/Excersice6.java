776b97bc5d4072e596117161a63e9593737c8200
parkeunyoung <yyoung112@naver.com>
2017-04-04 21:03:20 +0900

    3장 연습문제 6,7

--- StandardJava/src/eypark/chap03/Excersice6.java
+++ StandardJava/src/eypark/chap03/Excersice6.java
@@ -3,6 +3,6 @@ package eypark.chap03;
 public class Excersice6 {
 	public static void main(String[] args) {
 		int num = 24;
-		System.out.println(num%10 != 0 ? (num/10) * 10 + 10 : num/10);
+		System.out.println(num % 10 != 0 ? (num/10) * 10 + 10 - num : num/10);
 	}
 }
--- StandardJava/src/eypark/chap03/Excersice7.java
+++ StandardJava/src/eypark/chap03/Excersice7.java
@@ -2,9 +2,9 @@ package eypark.chap03;
 
 public class Excersice7 {
 	public static void main(String[] args) {
-		//c = 5/9 * (f-32) , 소수점 셋째자리 반올림
 		int fahrenheit = 100;
-		float celcius = 5/9 * (fahrenheit-32) ;
+//		float celcius = 5/9 * (fahrenheit-32);//셋째자리 반올림
+		float celcius = (int)((5/9f * (fahrenheit-32))*100+0.5)/100f;
 		
 		System.out.println("fahrenheit " + fahrenheit);
 		System.out.println("celcius " + celcius);
